48c64a462a71fa0508e8d5d029c0f3031584e790
Andre Luis Kunert <seuemail@example.com>
2025-08-03 23:54:56 -0300

    Bora Revisar Node01

--- 1_INTRO/1_primeiro_programa/arquivo.js
+++ 1_INTRO/1_primeiro_programa/arquivo.js
@@ -1,3 +1,5 @@
 console.log("Meu Primeiro Programa")
 console.log("þ Aqui rodar o primeiro Programa em Nodejs")
+console.log("Revisando... Node.js do Zero a Maestria com diversos projetos ")
 console.log("The End")
+
--- modulo01/node.js
+++
@@ -1,3 +0,0 @@
-comsole.log('---Brinquedo com node')
-console.log("Irei fazer uma revisão do Curso Node.js do Zero à Maestria")
-console.log("The End")
