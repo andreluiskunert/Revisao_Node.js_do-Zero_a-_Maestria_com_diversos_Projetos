f98b7c3ddcf2a7fac38cc29c0a3f4b1dac705b41
Andre Luis Kunert <seuemail@example.com>
2025-08-04 23:13:11 -0300

    Aprendendo mais sobre nodejs

---
+++ 2_FUNDAMENTOS/7_explorando_console/index.js
@@ -0,0 +1,27 @@
+// mais de um valor:
+const x = 7
+const y = 3
+const z = 5
+//
+const a = 'André'
+const l = 'Luis'
+const k = 'Kunert'
+
+console.log(x, y, z, a, l, k)
+// contagem de impressões
+console.count(`O valor de x é :${x}, contagem:`)
+console.count(`O valor de x é :${x}, contagem:`)
+console.count(`O valor de x é :${x}, contagem:`)
+console.count(`O valor de x é :${x}, contagem:`)
+console.count(`O valor de x é :${x}, contagem:`)
+console.count(`O valor de x é :${x}, contagem:`)
+console.count(`O valor de x é :${x}, contagem:`)
+console.count(`O valor de x é :${x}, contagem:`)
+console.count(`O valor de x é :${x}, contagem:`)
+console.count(`O valor de x é :${x}, contagem:`)
+// variavel entre string
+console.log(" O nome é %s %s,é Dev Full Stack .", a, l)
+// limpar o console
+setTimeout(() =>{
+ console.clear()
+}, 2000)
