dd12c3ebdd5335926d80a8818d70e3a070ce00d5
Andre Luis Kunert <seuemail@example.com>
2025-08-09 23:28:21 -0300

    Bora Codar com Node.js

--- 5_ACCOUNTS/index.js
+++ 5_ACCOUNTS/index.js
@@ -26,10 +26,56 @@ function operation() {
       },
     ])
     .then((answer) => {
-        const action = answer['action']
-        console.log(action)
+      const action = answer['action']
+      console.log(action)
       // Aqui você chama a função de acordo com a escolha
+      if (action === 'Criar Conta') {
+        createAccount()
+      }
 
     })
     .catch((err) => console.log(err));
+}
+// create account 
+function createAccount() {
+  console.log(chalk.bgGreen.black('Parabéns pela sua Escolha'))
+  console.log(chalk.green('Defina as opções da sua conta a seguir'))
+
+  buildAccount()
+}
+function buildAccount() {
+  inquirer
+    .prompt([
+      {
+        name: 'accountName',
+        message: 'Digite um nome para a sua conta:',
+      },
+    ])
+    .then((answer) => {
+      console.info(answer['accountName'])
+
+      const accountName = answer['accountName']
+
+      if (!fs.existsSync('accounts')) {
+        fs.mkdirSync('accounts')
+      }
+
+      if (fs.existsSync(`accounts/${accountName}.json`)) {
+        console.log(
+          chalk.bgRed.black('Esta conta já existe, escolha outro nome!'),
+        )
+        buildAccount(accountName)
+      }
+
+      fs.writeFileSync(
+        `accounts/${accountName}.json`,
+        '{"balance":0}',
+        function (err) {
+          console.log(err)
+        },
+      )
+
+      console.log(chalk.green('Parabéns, sua conta foi criada!'))
+      operation()
+    })
 }
