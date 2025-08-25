a3f5a8c75fccce1186127b48e8b916c555ee9dff
Andre Luis Kunert <seuemail@example.com>
2025-08-25 15:31:02 -0300

    Bora Codar com Node.js

---
+++ 9_SEQUELIZE/5_where/index.js
@@ -0,0 +1,54 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+const conn = require('./db/conn')
+const User = require('./model/User')
+const { where } = require('sequelize')
+const app = express()
+
+app.engine('handlebars', exphbs())
+app.set('view engine', 'handlebars')
+
+app.use(
+  express.urlencoded({
+    extended: true,
+  }),
+)
+
+app.use(express.json())
+
+app.use(express.static('public'))
+app.get('/users/create', (req, res)=>{
+  res.render('adduser')
+})
+app.post('/users/create', function (req, res) {
+  const name = req.body.name
+  const occupation = req.body.occupation
+ const newsletter = req.body.newsletter === 'on'
+
+
+  if (newsletter === 'on') {
+    newsletter = true
+  }
+
+  User.create({ name, occupation, newsletter })
+
+  res.redirect('/')
+})
+app.get('/users/:id', async (req, res) =>{
+  const id = req.params.id
+
+  const user = await User.findOne({raw: true, where:{id: id}})
+
+  res.render('userview', {user})
+})
+
+app.get('/', async (req, res) => {
+const users = await User.findAll({raw: true})
+console.log(users)
+  res.render('home', {users: users})
+})
+
+conn.sync().then(() =>{
+  app.listen(3000)
+})
+
---
+++ 9_SEQUELIZE/7_edit/model/User.js
@@ -0,0 +1,18 @@
+const { DataTypes } = require('sequelize')
+const db = require('../db/conn')
+
+const User = db.define('User', {
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  occupation: {
+    type: DataTypes.STRING,
+    allowNull: false,   // ⬅️ corrigido
+  },
+  newsletter: {
+    type: DataTypes.BOOLEAN,
+  },
+})
+
+module.exports = User   // ⬅️ garante exportação
