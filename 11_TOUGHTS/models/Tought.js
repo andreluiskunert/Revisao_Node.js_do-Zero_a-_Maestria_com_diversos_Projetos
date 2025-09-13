aa800926cd088c02450ce14573bead69e2a4c3b4
Andre Luis Kunert <seuemail@example.com>
2025-09-12 22:24:17 -0300

    bora codar com NodeJS_2025

--- 11_TOUGHTS/models/Tought.js
+++ 11_TOUGHTS/models/Tought.js
@@ -1,18 +1,17 @@
-const { DataTypes, Model } = require('sequelize')
+const { DataTypes } = require("sequelize");
 
-const db = require('../db/conn')
+const db = require("../db/conn");
 
-// -> User
-const User = require('./User')
+const User = require("../models/User");
 
-const Tought = db.define('Tought', {
-    title: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        require: true,
-    },
+const Tought = db.define("Tought", {
+  title: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+});
 
-})
- Tought.belongsTo(User)
- User.hasMany(Tought)
-module.exports = Tought
+Tought.belongsTo(User);
+User.hasMany(Tought);
+
+module.exports = Tought;
