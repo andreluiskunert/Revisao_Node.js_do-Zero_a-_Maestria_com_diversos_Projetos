69efa1ab87fea9b6a6aa8ba1b20a11c7d22cb278
Andre Luis Kunert <seuemail@example.com>
2025-09-16 23:04:42 -0300

    Bora Codar Node.js_2025

--- 11_TOUGHTS/controllers/AuthController.js
+++ 11_TOUGHTS/controllers/AuthController.js
@@ -1,8 +1,20 @@
+const User = require('../models/User')
+const bcrycpt = require('bcryptjs')
 module.exports = class AuthController{
   static login(req, res){
     res.render('auth/login')
   }
     static register(req, res){
     res.render('auth/register')
+  }
+  static async registerPost(req, res){
+    const { name, email, password, confirmpassword } = req.body
+    // password watch validation
+    if(password != confirmpassword){
+    // Mensagem pra o frontend
+     req.flash('message','As snhas são diferentes, tente novamente')
+     res.render('/auth/register')
+     return
+    }
   }
 }
