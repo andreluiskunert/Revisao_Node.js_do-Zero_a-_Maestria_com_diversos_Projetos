268875ca69cc238369b4307607859f40615d7438
Andre Luis Kunert <seuemail@example.com>
2025-09-19 22:35:43 -0300

    Bora Codar com Node.js_2025

--- 11_TOUGHTS/controllers/ToughtController.js
+++ 11_TOUGHTS/controllers/ToughtController.js
@@ -4,30 +4,29 @@ const Tought = require('../models/Tought')
 const User = require('../models/User')
 
 module.exports = class ToughtController {
-   static async showToughts(req, res) {
-  let search = ''
-  if (req.query.search) {
-    search = req.query.search
+  static async showToughts(req, res) {
+    let search = ''
+    if (req.query.search) {
+      search = req.query.search
+    }
+
+    let order = 'DESC'
+    if (req.query.order === 'old') {
+      order = 'ASC'
+    }
+
+    const toughtsData = await Tought.findAll({
+      include: User,
+      where: {
+        title: { [Op.like]: `%${search}%` }
+      },
+      order: [['createdAt', order]]
+    })
+
+    const toughts = toughtsData.map((result) => result.get({ plain: true }))
+    const toughtsQty = toughts.length
+
+    // 🔑 Alterado para apontar para a pasta correta da view
+    res.render('toughts/home', { toughts, search, toughtsQty })
   }
-
-  let order = 'DESC'
-  if (req.query.order === 'old') {
-    order = 'ASC'
-  }
-
-  const toughtsData = await Tought.findAll({
-    include: User,
-    where: {
-      title: { [Op.like]: `%${search}%` }
-    },
-    order: [['createdAt', order]]
-  })
-
-  const toughts = toughtsData.map((result) => result.get({ plain: true }))
-  const toughtsQty = toughts.length
-
-  // 🔑 Aqui o render manda as variáveis para o Handlebars
-  res.render('home', { toughts, search, toughtsQty })
-}
-
 }
