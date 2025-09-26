ad6ebf62e8db1d54c1c147b1d43459d61c6ea3a8
Andre Luis Kunert <seuemail@example.com>
2025-09-25 22:52:44 -0300

    Bora Codar Node,js_2025

--- 11_TOUGHTS/routes/authRoutes.js
+++ 11_TOUGHTS/routes/authRoutes.js
@@ -5,7 +5,5 @@ const AuthController = require('../controllers/AuthController')
 router.get('/login', AuthController.login)
 router.post('/login', AuthController.loginPost)
 router.get('/register', AuthController.register)
-router.post('/register', AuthController.registerPost)
-router.get('/logout', AuthController.logout)
-
+router.post('/register',AuthController.registerPost)
 module.exports = router
