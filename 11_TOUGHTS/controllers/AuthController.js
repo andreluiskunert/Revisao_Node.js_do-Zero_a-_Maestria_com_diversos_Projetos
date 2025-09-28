607a7195b8480914649434855ae662e0034297e6
Andre Luis Kunert <seuemail@example.com>
2025-09-27 23:54:49 -0300

    Codando com Node.JS_2025

--- 11_TOUGHTS/controllers/AuthController.js
+++ 11_TOUGHTS/controllers/AuthController.js
@@ -1,4 +1,4 @@
-const User = require('../models/User') // modelo Sequelize
+const User = require('../models/User')
 const bcrypt = require('bcryptjs')
 
 module.exports = class AuthController {
@@ -12,7 +12,6 @@ module.exports = class AuthController {
       req.flash('message', 'Preencha todos os campos!')
       return res.render('auth/login')
     }
-    // Aqui você faria validação real com User.findOne(...)
     req.flash('message', 'Login realizado com sucesso!')
     res.redirect('/')
   }
@@ -20,31 +19,14 @@ module.exports = class AuthController {
   static register(req, res) {
     res.render('auth/register')
   }
-
-  static async registerPost(req, res) {
-    const { name, email, password, confirmpassword } = req.body
-
+ static async registerPost(req, res){
+    const {name, email, password, confirmpassword} = req.body
     // Validação de senha
-    if (password !== confirmpassword) {
-      req.flash('message', 'As senhas não conferem, tente novamente!')
-      return res.render('auth/register')
+    if(password != confirmpassword){
+      req.flash('message', 'As senhas estão erradas')
+      res.render('auth/register')
+
+      return
     }
-
-    // Verificar se usuário já existe
-    const userExists = await User.findOne({ where: { email } })
-    if (userExists) {
-      req.flash('message', 'E-mail já cadastrado!')
-      return res.render('auth/register')
-    }
-
-    // Criptografar a senha
-    const salt = bcrypt.genSaltSync(10)
-    const hashedPassword = bcrypt.hashSync(password, salt)
-
-    // Criar usuário no banco
-    await User.create({ name, email, password: hashedPassword })
-
-    req.flash('message', 'Cadastro realizado com sucesso! Faça login.')
-    res.redirect('/login')
-  }
+   }
 }
