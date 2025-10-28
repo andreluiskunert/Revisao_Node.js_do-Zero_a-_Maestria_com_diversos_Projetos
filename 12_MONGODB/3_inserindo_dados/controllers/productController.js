5556eb9457e7aa74eb150a384868e3e3fa72acd1
Andre Luis Kunert <seuemail@example.com>
2025-10-27 22:46:22 -0300

    Bora Codar com Node.js_2025

--- 12_MONGODB/3_inserindo_dados/controllers/productController.js
+++ 12_MONGODB/3_inserindo_dados/controllers/productController.js
@@ -1,8 +1,28 @@
-exports.createProductForm = (req, res) => {
-  res.render('create') // se estiver usando handlebars ou ejs
-}
+const Product = require('../models/Product')
 
-exports.createProduct = (req, res) => {
-  // salvar no banco
-  res.send('Produto criado com sucesso!')
+module.exports = class ToughController {
+  static async showProducts(req, res) {
+    const products = await Product.getProducts()
+
+    console.log(products)
+
+    res.render('products/all', { products })
+  }
+
+  static createProduct(req, res) {
+    res.render('products/create')
+  }
+
+  static createProductPost(req, res) {
+    const name = req.body.name
+    const price = req.body.price
+    const description = req.body.description
+    const image = req.body.image
+
+    const product = new Product(name, price, description, image)
+
+    product.save()
+
+    res.redirect('/')
+  }
 }
