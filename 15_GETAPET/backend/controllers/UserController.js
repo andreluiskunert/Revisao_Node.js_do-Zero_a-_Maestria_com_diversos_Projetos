8bf48718a803058d2cb20a18c146b11a74edfcf6
Andre Luis Kunert <seuemail@example.com>
2025-11-27 23:36:16 -0300

    Bora La coda com Node.js_2025

--- 15_GETAPET/backend/controllers/UserController.js
+++ 15_GETAPET/backend/controllers/UserController.js
@@ -1,6 +1,98 @@
 const User = require('../models/User')
 module.exports = class UserController {
   static async register(req, res) {
-    res.json('Ola Get A Pet')
+    // const name = req.body.name
+    // const email = req.body.email
+    // const phone = req.body.phone
+    // const password = req.body.password
+    // const confirmpassword = req.body.confirmpassword
+    const {name, email, phone, password, confirmpassword }= req.body
+    // validações:
+    if (!name) {
+      res.status(422).json({ message: 'O nome é obrigatório!' })
+      return
+    }
+
+    if (!email) {
+      res.status(422).json({ message: 'O e-mail é obrigatório!' })
+      return
+    }
+
+    if (!phone) {
+      res.status(422).json({ message: 'O telefone é obrigatório!' })
+      return
+    }
+
+    if (!password) {
+      res.status(422).json({ message: 'A senha é obrigatória!' })
+      return
+    }
+
+    if (!confirmpassword) {
+      res.status(422).json({ message: 'A confirmação de senha é obrigatória!' })
+      return
+    }
+
+    if (password != confirmpassword) {
+      res
+        .status(422)
+        .json({ message: 'A senha e a confirmação precisam ser iguais!' })
+      return
+    }
+
+    // check if user exists
+    const userExists = await User.findOne({ email: email })
+
+    if (userExists) {
+      res.status(422).json({ message: 'Por favor, utilize outro e-mail!' })
+      return
+    }
+
+    // create password
+    const salt = await bcrypt.genSalt(12)
+    const passwordHash = await bcrypt.hash(password, salt)
+
+    // create user
+    const user = new User({
+      name: name,
+      email: email,
+      phone: phone,
+      password: passwordHash,
+    })
+
+    try {
+      const newUser = await user.save()
+
+      await createUserToken(newUser, req, res)
+    } catch (error) {
+      res.status(500).json({ message: error })
+    }
+  }
+
+  static async login(req, res) {
+    const email = req.body.email
+    const password = req.body.password
+
+    if (!email) {
+      res.status(422).json({ message: 'O e-mail é obrigatório!' })
+      return
+    }
+
+    if (!password) {
+      res.status(422).json({ message: 'A senha é obrigatória!' })
+      return
+    }
+
+    // check if user exists
+    const user = await User.findOne({ email: email })
+
+    if (!user) {
+      return res
+        .status(422)
+        .json({ message: 'Não há usuário cadastrado com este e-mail!' })
+    }
+
+
   }
 }
+
