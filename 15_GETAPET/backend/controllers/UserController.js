50ca5647bc4642b8fa7753fb98440c9ff9b5c056
Andre Luis Kunert <seuemail@example.com>
2026-01-06 22:35:28 -0300

    Bora La Codar Node.js_2026

--- 15_GETAPET/backend/controllers/UserController.js
+++ 15_GETAPET/backend/controllers/UserController.js
@@ -58,6 +58,92 @@ module.exports = class UserController {
     } catch(error){
       res.status(500).json({message: error})
     }
-
   }
+  // Terça-feira, 06/01/2026_de noite
+  static async login(req, res) {
+    const {email, password} = req.body
+     if (!email) {
+      res.status(422).json({ message: 'O Email é obrigatório:' })
+      return
+    }
+     if (!password) {
+      res.status(422).json({ message: 'A Senha é obrigatória:' })
+      return
+    }
+
+    // check if user exists
+    const user = await User.findOne({ email: email })
+
+    if (!user) {
+      return res
+        .status(422)
+        .json({ message: 'Não há usuário cadastrado com este e-mail!' })
+    }
+
+    // check if password match
+    const checkPassword = await bcrypt.compare(password, user.password)
+
+    if (!checkPassword) {
+      return res.status(422).json({ message: 'Senha inválida' })
+    }
+
+    await createUserToken(user, req, res)
+  }
+
+  static async checkUser(req, res) {
+    let currentUser
+
+    console.log(req.headers.authorization)
+
+    if (req.headers.authorization) {
+      const token = getToken(req)
+      const decoded = jwt.verify(token, 'nossosecret')
+
+      currentUser = await User.findById(decoded.id)
+
+      currentUser.password = undefined
+    } else {
+      currentUser = null
+    }
+
+    res.status(200).send(currentUser)
+  }
+
+  static async getUserById(req, res) {
+    const id = req.params.id
+
+    const user = await User.findById(id)
+
+    if (!user) {
+      res.status(422).json({ message: 'Usuário não encontrado!' })
+      return
+    }
+
+    res.status(200).json({ user })
+  }
+
+  static async editUser(req, res) {
+    const token = getToken(req)
+
+    //console.log(token);
+
+    const user = await getUserByToken(token)
+
+    // console.log(user);
+    // console.log(req.body)
+    // console.log(req.file.filename)
+
+    const name = req.body.name
+    const email = req.body.email
+    const phone = req.body.phone
+    const password = req.body.password
+    const confirmpassword = req.body.confirmpassword
+
+    let image = ''
+
+    if (req.file) {
+      image = req.file.filename
+    }
+  }
+  
 }
