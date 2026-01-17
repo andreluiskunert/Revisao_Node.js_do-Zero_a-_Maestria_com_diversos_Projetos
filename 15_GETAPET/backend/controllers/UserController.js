8997d34f4ed677c189baa4f3f2b0dfe90ca085a1
Andre Luis Kunert <https://github.com/andreluiskunert/Curso_Angular_2_v19_2026>
2026-01-16 23:02:47 -0300

    bora la codar Node.js_2026

--- 15_GETAPET/backend/controllers/UserController.js
+++ 15_GETAPET/backend/controllers/UserController.js
@@ -2,33 +2,13 @@
 const User = require('../models/User')
 module.exports = class UserController {
     static async register(req, res){
-    //   const name = req.body.name
-    //   const email = req.body.email
-    //   const phone = req.body.phone
-    //   const password = req.body.password
-    //   const confirmpassword = req.body.confirmpassword
-
-      const {name , email, phone, password, confirmpassword} = req.body
-        // validações de erros
-        if(!name) {
-            res.status(422).json({message: 'O nome é  obrigatório: '})
-            return
-        }
-        if(!email){
-            res.status(422).json({message: 'O email é  obrigatório: '})
-            return
-        }
-        if(!phone){
-            res.status(422).json({message: 'O telefone é obrigatório: '})
-            return
-        }
-        if(!password){
-            res.status(422).json({message: 'A senha é  obrigatória: '})
-            return
-        }
-        if(!confirmpassword){
-            res.status(422).json({message: 'A confirmação de senha  é  obrigatória: '})
-            return
-        }
+       const {name, email, phone, password, confirmpassword} = req.body
+       if(!name){
+        res.status(422).json({message: 'O nome é obrigatório:'})
+        return
+        // revisão amanhã...
+       }
     }
-}
+    
+  }
+
