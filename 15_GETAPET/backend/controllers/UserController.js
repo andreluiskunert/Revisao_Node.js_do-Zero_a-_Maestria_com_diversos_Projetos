52787ad5d894790c9ce840043e84e225d2e29c94
Andre Luis Kunert <andreluiskunert@gmail.com>
2026-01-27 22:19:36 -0300

    bora la Estudar Node.js_2026_backend

---
+++ 15_GETAPET/backend/controllers/UserController.js
@@ -0,0 +1,8 @@
+// Terça-feira,27/01/2026
+const User = require('../models/User')
+
+module.exports = class UserController {
+    static async register(re, res){
+        res.json('Olá Get a Pet')
+    }
+}
