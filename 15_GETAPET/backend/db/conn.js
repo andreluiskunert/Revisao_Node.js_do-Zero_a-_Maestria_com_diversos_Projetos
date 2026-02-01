a215a0cc520886cfe15025f00494da3e95b2a027
Andre Luis Kunert <andreluiskunert@gmail.com>
2026-01-31 23:34:44 -0300

    bora Estudar Node.js_2026_Backend_

--- 15_GETAPET/backend/db/conn.js
+++ 15_GETAPET/backend/db/conn.js
@@ -1,14 +1,11 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
-async function connect() {
-  try {
-    await mongoose.connect('mongodb://localhost:27017/getapet')
-    console.log('Conectado ao MongoDB com Mongoose!')
-  } catch (error) {
-    console.error('Erro ao conectar no MongoDB:', error)
-  }
+async function main() {
+  await mongoose.connect('mongodb://localhost:27017/getapet');
+  console.log('Conectou ao Mongoose');
+
 }
 
-connect()
+main().catch((err) => console.log(err));
 
-module.exports = mongoose
+module.exports = mongoose;
--- 15_GETAPET/backend/index.js
+++ 15_GETAPET/backend/index.js
@@ -1,22 +1,43 @@
-const express = require('express')
-const cors = require('cors')
+// const express = require('express')
+// const cors = require('cors')
 
-const app = express()
+// const app = express()
 
-// Config JSON response
-app.use(express.json())
+// // Config JSON response
+// app.use(express.json())
 
-// Solve CORS
-app.use(cors({ credentials: true, origin: 'http://localhost:3000' }))
+// // Solve CORS
+// app.use(cors({ credentials: true, origin: 'http://localhost:3000' }))
+
+// // Public folder for images
+// app.use(express.static('public'))
+
+// // Routes
+// // Sabádo, 31/01/2026_de manha
+// const UserRoutes = require('./routes/UserRoutes')
+// app.use('/users', UserRoutes)
+
+// //  Estou buscando conexão com o Banco de Dados usando https://cloud.mongodb.com/v2#/org/684e1fcfc80840285f0b1f04/projects
+
+// app.listen(5000)
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+const UserRoutes = require('./routes/UserRoutes');
+// const PetRoutes = require('./routes/PetRoutes');
+
+// config json response
+app.use(express.json());
+
+// solve cors
+app.use(cors({ credentials: true, origin: 'http://localhost:3000'}));
 
 // Public folder for images
-app.use(express.static('public'))
+app.use(express.static('public'));
 
-// Routes
-// Sabádo, 31/01/2026_de manha
-const UserRoutes = require('./routes/UserRoutes')
-app.use('/users', UserRoutes)
+// routes
+app.use('/users', UserRoutes);
+// app.use('/pets', PetRoutes);
 
-//  Estou buscando conexão com o Banco de Dados usando https://cloud.mongodb.com/v2#/org/684e1fcfc80840285f0b1f04/projects
-
-app.listen(5000)
+app.listen(5000);
